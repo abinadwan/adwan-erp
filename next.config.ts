556a7642fbefd56beffb7326a6ae84e443eee49d
abinadwan <abinadwan@gmail.com>
2025-09-05 00:52:52 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,18 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  experimental: {
-    ppr: true,
-  },
+  output: 'export',   // مهم جداً للتصدير لمجلد out
+  trailingSlash: true, // يخلي كل الروابط تنتهي بـ / عشان ما يخرب التصفح في Pages
   images: {
+    unoptimized: true, // GitHub Pages ما يدعم تحسين الصور السيرفري
     remotePatterns: [
       {
         hostname: 'avatar.vercel.sh',
       },
     ],
+  },
+  experimental: {
+    ppr: true, // تقدرين تخليه، بس ما راح يشتغل فعلياً مع static export
   },
 };
 
