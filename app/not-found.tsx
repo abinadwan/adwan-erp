35d2e5f04d9fe54eeae630ac76826095da5867fa
abinadwan <abinadwan@gmail.com>
2025-09-05 23:05:44 +0300

    Delete app directory

--- app/not-found.tsx
+++
@@ -1,8 +0,0 @@
-export default function NotFound() {
-  return (
-    <div className="p-4">
-      <h1 className="text-2xl font-bold">Not Found</h1>
-      <p>The requested resource was not found.</p>
-    </div>
-  );
-}
