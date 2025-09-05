d15d8a8324b0aaad15fa62aa28902d911712eb66
abinadwan <abinadwan@gmail.com>
2025-09-05 14:18:40 +0300

    feat: configure next-intl for app router (#9)

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
-};
+import createNextIntlPlugin from 'next-intl/plugin';
 
-export default nextConfig;
+const withNextIntl = createNextIntlPlugin('./next-intl.config.js');
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+export default withNextIntl(nextConfig);
