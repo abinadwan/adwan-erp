92fafc86df2151526b804c0040d1666c928fa418
abinadwan <abinadwan@gmail.com>
2025-09-05 23:06:06 +0300

    Delete lib directory

--- lib/auth.ts
+++
@@ -1,9 +0,0 @@
-import { supabaseServer } from './supabase/server';
-
-export async function requireUser() {
-  const supabase = supabaseServer();
-  const {
-    data: { user },
-  } = await supabase.auth.getUser();
-  return user;
-}
--- lib/utils/rtl.ts
+++
@@ -1,6 +0,0 @@
-const RTL_LOCALES = new Set(['ar', 'fa', 'he', 'ur']);
-
-export function isRTL(locale: string) {
-  const lang = locale.split('-')[0];
-  return RTL_LOCALES.has(lang);
-}
