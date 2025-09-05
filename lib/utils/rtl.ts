f6df8af058b0f4ee68c04d92bd95a733b772802e
abinadwan <abinadwan@gmail.com>
2025-09-05 22:46:36 +0300

    Support multiple RTL locales

--- lib/utils/rtl.ts
+++ lib/utils/rtl.ts
@@ -1,3 +1,6 @@
+const RTL_LOCALES = new Set(['ar', 'fa', 'he', 'ur']);
+
 export function isRTL(locale: string) {
-  return locale === 'ar';
+  const lang = locale.split('-')[0];
+  return RTL_LOCALES.has(lang);
 }
