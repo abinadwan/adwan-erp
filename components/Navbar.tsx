1f626ef444e2b72f4e86a27c062c5d44a881f264
abinadwan <abinadwan@gmail.com>
2025-09-05 19:50:49 +0300

    Render navbar globally (#18)

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,7 @@
 'use client';
 import Link from 'next/link';
 import LangSwitcher from './LangSwitcher';
-in
+ain
   return (
     <nav className="flex items-center justify-between p-4 bg-white shadow">
       <Link href={`/${locale}`} className="font-bold">
@@ -9,7 +9,7 @@ in
       </Link>
       <div className="flex items-center gap-4">
         <Link href={`/${locale}`} className="hover:underline">
-main
+> main
         </Link>
         <LangSwitcher />
       </div>
