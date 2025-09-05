ec1421d029d8fdb38a2c1d40509fd0f6a5ca2af3
abinadwan <abinadwan@gmail.com>
2025-09-05 20:09:35 +0300

    Update Navbar.tsx
    Fix bugs

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,15 +1,21 @@
 'use client';
+
 import Link from 'next/link';
 import LangSwitcher from './LangSwitcher';
-main
+import { useLocale } from 'next-intl';
+
+export default function Navbar() {
+  const locale = useLocale();
+
   return (
     <nav className="flex items-center justify-between p-4 bg-white shadow">
       <Link href={`/${locale}`} className="font-bold">
         ERP-HR
       </Link>
+
       <div className="flex items-center gap-4">
         <Link href={`/${locale}`} className="hover:underline">
-> main
+          Home
         </Link>
         <LangSwitcher />
       </div>
