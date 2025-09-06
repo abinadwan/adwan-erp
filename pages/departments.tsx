5022bf6c04408f1cc318bcb211a817f520c51a92
abinadwan <abinadwan@gmail.com>
2025-09-07 00:51:55 +0300

    Add departments page component

---
+++ pages/departments.tsx
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+
+export default function DepartmentsPage() {
+  return (
+    <div>
+      <h1>Departments</h1>
+      <Link href="/">Back to Dashboard</Link>
+    </div>
+  );
+}
